dae5395c95498f9eeaccd69fe3dbe78b11b37724
Ricardo Rodrigues <93055503+rrccdd6@users.noreply.github.com>
2022-08-03 21:01:18 -0300

    Versão 1.6
    Captura de login completa e alerta exibido

--- js/login.js
+++ js/login.js
@@ -1,14 +1,46 @@
+function submit_by_id() {
+    var usuario = document.getElementById("usuario").value;
+    var senha = document.getElementById("senha").value;
+    if (validation()) // Calling validation function
+    {
+        document.getElementById("formulario").submit(); //form submission
+        alert(" Usuário : " + usuario + " Senha : " + senha + " " + " Entrada permitida");
+    }
+}
+// Name and Email validation Function.
+function validation() {
+    var usuario = document.getElementById("usuario").value;
+    var senha = document.getElementById("senha").value;
+    if (usuario === '' || senha === '') {
+        alert("Please fill all fields...!!!!!!");
+        return false;
+    } else {
+        return true;
+    }
+}
+//let usuario = document.getElementById(usuario).value;
+//let senha = document.getElementById(senha).value;
+
+//app.post('/add', function(req, res)) {
+//    res.send('formulario enviado');
+//    console.log('Sucesso');
+//}
+
 function login() {
 
-    var login = document.getElementById(usuario).value;
-    var senha = document.getElementById(senha).value;
+    if (usuario == 'admin' && senha == 'admin') {
+        console.log('Sucesso');
 
-
-    if (login == 'admin' && senha == 'admin') {
-        alert("Sucesso");
     } else {
-        alert("Acesso negado");
+        console.log("Acesso negado");
+
     }
 
+    newFunction();
 
+    return;
+}
+
+function newFunction() {
+    localStorage.setItem(login, JSON.stringify(senha));
 }
